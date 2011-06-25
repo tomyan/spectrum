29138816cde3d8a959684ed5333fc9534e8a12c4
Richard Hodgson <rightaboutnow@gmail.com>
2011-06-25 10:46:29 -0400

    Updated renderer test for new api. Added to test suite

---
+++ tests/renderer.js
@@ -0,0 +1,35 @@
+var litmus   = require('litmus'),
+    Spectrum = require('../lib/spectrum');
+
+exports.test = new litmus.Test('Spectrum renderer', function () {
+    this.plan(2);
+    
+    console.log(Spectrum);
+    
+    var spectrum = new Spectrum.Renderer(__dirname + '/root');
+        test = this;
+    
+    function testTemplate (path, params, output, message) {
+        test.async('render shortcut', function (handle) {
+            spectrum.render(path, params).then(
+                function (rendered) {
+                    test.is(rendered, output, message);
+                    handle.finish();
+                }
+            );
+        });
+    }
+    testTemplate(
+        '/content.spv',
+        {},
+        'template containing simple content\n',
+        'run template with simple content'
+    );
+
+    testTemplate(
+        '/content-with-params.spv',
+        { 'aParam' : 'hello' },
+        'Content with "hello".\n',
+        'run template with view parameter'
+    );
+});
--- tests/suite.js
+++ tests/suite.js
@@ -2,8 +2,8 @@
 var litmus = require('litmus');
 
 exports.test = new litmus.Suite('Spectrum Test Suite', [
-    require('./parser').test // ,
+    require('./parser').test,
+    require('./renderer').test
 //    require('./generator').test,
-//    require('./processor').test,
 //    require('./templateroot').test
 ]);
