2787d04379fc06b1b5c37d85084631d6bc762e01
Richard Hodgson <rightaboutnow@gmail.com>
2011-06-25 11:34:11 -0400

    Tests for template.render method.

--- tests/suite.js
+++ tests/suite.js
@@ -3,7 +3,7 @@ var litmus = require('litmus');
 
 exports.test = new litmus.Suite('Spectrum Test Suite', [
     require('./parser').test,
-    require('./renderer').test
-//    require('./generator').test,
+    require('./renderer').test,
+    require('./generator').test
 //    require('./templateroot').test
 ]);
